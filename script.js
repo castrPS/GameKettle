7385fa6457390d0fca4d187dc733cb2811e48a07
Cris Correa <ecmac@cin.ufpe.br>
2018-12-08 17:35:42 -0300

    3 jogos somando os resultados sem filtro

--- script.js
+++ script.js
@@ -1,23 +1,56 @@
-requestSearch(){
+/**
+   * Performs a search request against an Elasticsearch server.
+   * @param {string} needle
+   *   The string to search for.
+   * @param {string} filter
+   *   A string to use to filter by type. For example: 'article';
+   */
+  function doSearch (needle) {
+    var searchHost = 'http://localhost:9200/reviews/_search';
+	var body = {
+      'size': 20
+    };
+	var query = {
+        'bool': {}
+    };
+    query.bool.must = {
+    	'match': {
+            "comments": needledrag
+        }
+    };
+	
+	body.query = query;
 
-	var ajax = new XMLHttpRequest();
+    // Perform the request.
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open('POST', searchHost, false);
+    xmlHttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    xmlHttp.send(JSON.stringify(body));
+    var response = JSON.parse(xmlHttp.responseText);
 
-	// Seta tipo de requisição e URL com os parâmetros
-	ajax.open("GET", "minha-url-api.com/?name=Henry&lastname=Ford", true);
+    // Print results on screen.
+    var output = response.hits.hits;
+    document.getElementById('total').innerHTML = '<h2>Showing ' + response.hits.hits.length + ' results</h2>';
+    document.getElementById('hits').innerHTML = output;
+  }
 
-	// Envia a requisição
-	ajax.send();
+  /**
+   * Helper function to obtain the value of a set of radio buttons.
+   *
+   * @param {string} name
+   *   The name of the radio elements.
+   * @returns {string}
+   *   The selected value or an empty string if nothing was selected.
+   */
+  function getRadioButtonValue (name) {
+    var radios = document.getElementsByName(name);
 
-	// Cria um evento para receber o retorno.
-	ajax.onreadystatechange = function() {
-	  
-	  // Caso o state seja 4 e o http.status for 200, é porque a requisiçõe deu certo.
-		if (ajax.readyState == 4 && ajax.status == 200) {
-	    
-			var data = ajax.responseText;
-			
-	    // Retorno do Ajax
-			console.log(data);
-		}
-	}
-}
+    for (var i = 0, length = radios.length; i < length; i++) {
+      if (radios[i].checked) {
+        return radios[i].value;
+      }
+    }
+    return '';
+  }
+
+  doSearch('', '');
